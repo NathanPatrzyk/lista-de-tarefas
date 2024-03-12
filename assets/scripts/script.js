34837a9dc7263319ee8d899e1ce19ace67fc7183
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-03-11 22:15:39 -0300

    Continuando script

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -12,14 +12,34 @@ buttonAdd.addEventListener("click", event => {
     else
     {
         let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+
         values.push({
-            name: inputTask.value
+            name: inputTask.value,
+            status: 'Pendente'
         })
         
         localStorage.setItem(localStorageKey,JSON.stringify(values))
 
-        let tasks = document.getElementById('tasks')
+        showValues()
+        
     }
-    inputTask.value = ''
-
 })
+
+function showValues(){
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+
+    let tasks = document.getElementById('tasks')
+    
+    tasks.innerHTML = ''
+    for(let i = 0; i < values.length; i++){
+        tasks.innerHTML += `<div class="task">
+            <span>
+                <svg xmlns="http://www.w3.org/2000/svg" height="24" width="21" viewBox="0 0 448 512"><path fill="#cbd5e1" d="M384 80c8.8 0 16 7.2 16 16V416c0 8.8-7.2 16-16 16H64c-8.8 0-16-7.2-16-16V96c0-8.8 7.2-16 16-16H384zM64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64z"/>
+                </svg>
+            </span>
+            <p>${values[i]['name']}</p>
+        </div>`
+    }
+}
+
+showValues()
