0cec868b3104a6cf0a704a18b953f89a74f574ab
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-03-13 20:08:06 -0300

    Finalizando script

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -50,26 +50,53 @@ function showTasks() {
 
     tasks.forEach((task) => {
 
-        divTasks.innerHTML += `
-            <div class="task" id="task-${task['name']}">
-                <span class="do-task" id='do-task-${task['name']}' onclick='doTask("${task['name']}")'>
-                    <svg xmlns="http://www.w3.org/2000/svg" height="24" width="21" viewBox="0 0 448 512">
-                        <path fill="#94a3b8" d="M384 80c8.8 0 16 7.2 16 16V416c0 8.8-7.2 16-16 16H64c-8.8 0-16-7.2-16-16V96c0-8.8 7.2-16 16-16H384zM64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64z"/>
-                    </svg>
-                </span>
-                <p>${task['name']}</p>
-                <span class="edit-task" onclick='openInputEditTask("${task['name']}")'>
-                    <svg xmlns="http://www.w3.org/2000/svg" height="24" width="21" viewBox="0 0 448 512">
-                        <path fill="#3b82f6" d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zM325.8 139.7l14.4 14.4c15.6 15.6 15.6 40.9 0 56.6l-21.4 21.4-71-71 21.4-21.4c15.6-15.6 40.9-15.6 56.6 0zM119.9 289L225.1 183.8l71 71L190.9 359.9c-4.1 4.1-9.2 7-14.9 8.4l-60.1 15c-5.5 1.4-11.2-.2-15.2-4.2s-5.6-9.7-4.2-15.2l15-60.1c1.4-5.6 4.3-10.8 8.4-14.9z"/>
-                    </svg>
-                </span>
-                <span class="delete-task" onclick='deleteTask("${task['name']}")'>
-                    <svg xmlns="http://www.w3.org/2000/svg" height="24" width="21" viewBox="0 0 448 512">
-                        <path fill="#ef4444" d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm79 143c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"/>
-                    </svg>
-                </span>
-            </div>
-        `
+        if (task["status"] == "Pendente") {
+
+            divTasks.innerHTML += `
+                <div class="task" id="task-${task['name']}">
+                    <span class="do-task" onclick='doTask("${task['name']}")'>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24" width="21" viewBox="0 0 448 512">
+                            <path fill="#94a3b8" d="M384 80c8.8 0 16 7.2 16 16V416c0 8.8-7.2 16-16 16H64c-8.8 0-16-7.2-16-16V96c0-8.8 7.2-16 16-16H384zM64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64z"/>
+                        </svg>
+                    </span>
+                    <p>${task['name']}</p>
+                    <span class="edit-task" onclick='openInputEditTask("${task['name']}")'>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24" width="21" viewBox="0 0 448 512">
+                            <path fill="#3b82f6" d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zM325.8 139.7l14.4 14.4c15.6 15.6 15.6 40.9 0 56.6l-21.4 21.4-71-71 21.4-21.4c15.6-15.6 40.9-15.6 56.6 0zM119.9 289L225.1 183.8l71 71L190.9 359.9c-4.1 4.1-9.2 7-14.9 8.4l-60.1 15c-5.5 1.4-11.2-.2-15.2-4.2s-5.6-9.7-4.2-15.2l15-60.1c1.4-5.6 4.3-10.8 8.4-14.9z"/>
+                        </svg>
+                    </span>
+                    <span class="delete-task" onclick='deleteTask("${task['name']}")'>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24" width="21" viewBox="0 0 448 512">
+                            <path fill="#ef4444" d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm79 143c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"/>
+                        </svg>
+                    </span>
+                </div>
+            `
+
+        }
+        else if (task["status"] == "Feito") {
+
+            divTasks.innerHTML += `
+                <div class="task done" id="task-${task['name']}">
+                    <span class="do-task" onclick='doTask("${task['name']}")'>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24" width="21" viewBox="0 0 448 512">
+                            <path fill="#94a3b8" d="M384 80c8.8 0 16 7.2 16 16V416c0 8.8-7.2 16-16 16H64c-8.80-16-7.2-16-16V96c0-8.8 7.2-16 16-16H384zM64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 6464 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64z"/>
+                        </svg>
+                    </span>
+                    <p>${task['name']}</p>
+                    <span class="edit-task" onclick='openInputEditTask("${task['name']}")'>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24" width="21" viewBox="0 0 448 512">
+                            <path fill="#3b82f6" d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 064-28.7 64-64V96c0-35.3-28.7-64-64-64H64zM325.8 139.7l14.4 14.4c15.6 15.6 15.6 40.9 056.6l-21.4 21.4-71-71 21.4-21.4c15.6-15.6 40.9-15.6 56.6 0zM119.9 289L225.1 183.8l7171L190.9 359.9c-4.1 4.1-9.2 7-14.9 8.4l-60.1 15c-5.5 1.4-11.2-.2-15.2-4.2s-5.6-9.7-42-15.2l15-60.1c1.4-5.6 4.3-10.8 8.4-14.9z"/>
+                        </svg>
+                    </span>
+                    <span class="delete-task" onclick='deleteTask("${task['name']}")'>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24" width="21" viewBox="0 0 448 512">
+                            <path fill="#ef4444" d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 064-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm79 143c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c94-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 94-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-94-24.6 0-33.9z"/>
+                        </svg>
+                    </span>
+                </div>
+            `
+        }
 
     })
 
@@ -126,17 +153,16 @@ function confirmEditTask(name) {
 }
 
 function doTask(name) {
-    spanDoTask = document.getElementById(`do-task-${name}`)
+    let tasks = JSON.parse(localStorage.getItem(localStorageKey) || "[]").filter(task => task.name !== name)
 
-    spanDoTask.innerHTML = ''
+    tasks.push({
+        name: name,
+        status: 'Feito'
+    })
 
-    spanDoTask.innerHTML = `
-        
-    `
-    
+    localStorage.setItem(localStorageKey, JSON.stringify(tasks))
 
     showTasks()
-
 }
 
 showTasks()
